924e3324b78db31500218d3e07dd353e1f863f2f
Zheng <aheze@getfind.app>
2022-10-12 11:53:47 -0700

    Initial Commit

---
+++ Conjugator/ConjugatorApp.swift
@@ -0,0 +1,19 @@
+//
+//  ConjugatorApp.swift
+//  Conjugator
+//
+//  Created by A. Zheng (github.com/aheze) on 10/12/22.
+//  Copyright © 2022 A. Zheng. All rights reserved.
+//
+    
+
+import SwiftUI
+
+@main
+struct ConjugatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
