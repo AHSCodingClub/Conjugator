4c7c0a8fcfbc528931fc342272d4a6972eb6b40b
H. Kamran <hkamran@unisontech.org>
2022-10-12 19:02:48 -0700

    Run SwiftFormat

--- Conjugator/ContentView.swift
+++ Conjugator/ContentView.swift
@@ -5,7 +5,6 @@
 //  Created by A. Zheng (github.com/aheze) on 10/12/22.
 //  Copyright © 2022 A. Zheng. All rights reserved.
 //
-    
 
 import SwiftUI
 
--- Conjugator/ViewModel.swift
+++ Conjugator/ViewModel.swift
@@ -5,10 +5,9 @@
 //  Created by A. Zheng (github.com/aheze) on 10/12/22.
 //  Copyright © 2022 A. Zheng. All rights reserved.
 //
-    
 
 import SwiftUI
 
 class ViewModel: ObservableObject {
-    
+
 }
