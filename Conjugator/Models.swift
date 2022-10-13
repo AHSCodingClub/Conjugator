781319169f24770e4e9c83ae7053c38051743bae
H. Kamran <hkamran@unisontech.org>
2022-10-12 19:03:08 -0700

    Move the `Challenge` struct into the `Level` struct

--- Conjugator/Models.swift
+++ Conjugator/Models.swift
@@ -9,29 +9,34 @@
 import SwiftUI
 
 struct Level {
+    struct Challenge {
+        var verb: String
+
+        /**
+     Should be of length 6 and contain forms for **yo, tú, él/ella/usted, nosotros/nosotras, vosotros/vosotras, and ellos/ellas/ustedes**
+     */
+        var forms: [String]
+    }
+
     var title: String
     var description: String
+    /// There should be at least two challenges
     var challenges: [Challenge]
 }
 
-struct Challenge {
-    var verb: String
-
-    /**
-     Should be of length 6 and contain forms for: **yo, tú, el, nosotros, vosotros, ellos**
-     */
-    var forms: [String]
-}
-
+// TOOD: Remove after loading logic is fully implemented
 extension Level {
     static let testingLevels: [Level] = [
         Level(
             title: "Nivel Fácil (presente)",
             description: "Algunos verbos fáciles en tiempo presente",
             challenges: [
-                Challenge(verb: "comer", options: ["como", "comes", "come", "comemos", "coméis", "comen"]),
-                Challenge(verb: "beber", options: ["bebo", "bebes", "bebe", "bebemos", "bebéis", "beben"]),
-                Challenge(verb: "andar", options: ["ando", "andas", "anda", "andamos", "andáis", "andan"]),
+                Challenge(
+                    verb: "comer", options: ["como", "comes", "come", "comemos", "coméis", "comen"]),
+                Challenge(
+                    verb: "beber", options: ["bebo", "bebes", "bebe", "bebemos", "bebéis", "beben"]),
+                Challenge(
+                    verb: "andar", options: ["ando", "andas", "anda", "andamos", "andáis", "andan"]),
             ]
         ),
 
@@ -39,8 +44,12 @@ extension Level {
             title: "Go-go verbs (presente)",
             description: "Más dificles",
             challenges: [
-                Challenge(verb: "tener", options: ["tengo", "tienes", "tiene", "tenemos", "tenéis", "tienen"]),
-                Challenge(verb: "poner", options: ["pongo", "pones", "pone", "ponemos", "ponéis", "ponen"]),
+                Challenge(
+                    verb: "tener",
+                    options: ["tengo", "tienes", "tiene", "tenemos", "tenéis", "tienen"]),
+                Challenge(
+                    verb: "poner",
+                    options: ["pongo", "pones", "pone", "ponemos", "ponéis", "ponen"]),
             ]
         ),
     ]
